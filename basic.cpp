85e10cff2d845a524810dec0169e85e68c245843
aryan <aryanss14a7@gmail.com>
2023-09-29 23:59:43 +0530

    two and one

---
+++ basic.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a ;
+    cin>> a;
+    switch(a){
+        case 1:
+        cout <<"option A";
+        break;
+        case 2:
+        cout<<"option B";
+    }
+return 0;
+}
+
